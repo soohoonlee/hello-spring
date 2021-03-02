19ff36f5c54c1d647e299f0ea1d5a7fec78ed630
soohoonlee <jjinimania@gmail.com>
2021-03-02 23:50:03 +0900

    api

--- src/main/kotlin/hello/hellospring/controller/HelloController.kt
+++ src/main/kotlin/hello/hellospring/controller/HelloController.kt
@@ -5,6 +5,7 @@ import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
 class HelloController {
@@ -20,4 +21,18 @@ class HelloController {
         model["name"] = name
         return "hello-template"
     }
+    
+    @GetMapping("hello-string")
+    @ResponseBody
+    fun helloString(@RequestParam(value = "name") name: String): String {
+        return "hello $name"
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    fun helloApi(@RequestParam(value = "name") name: String): Hello {
+        return Hello(name)
+    }
+
+    data class Hello(var name: String)
 }
