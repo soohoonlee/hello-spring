291f87626e97f65cc7b8c31a696f75e891dee5f7
soohoonlee <jjinimania@gmail.com>
2021-03-04 00:21:12 +0900

    member query

--- src/main/kotlin/hello/hellospring/controller/MemberController.kt
+++ src/main/kotlin/hello/hellospring/controller/MemberController.kt
@@ -3,6 +3,8 @@ package hello.hellospring.controller
 import hello.hellospring.domain.Member
 import hello.hellospring.service.MemberService
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 
@@ -11,7 +13,7 @@ class MemberController(val memberService: MemberService) {
 
     @GetMapping("/members/new")
     fun createForm(): String {
-        return "members/createMemberForm"
+        return "members/create-member-form"
     }
 
     @PostMapping("/members/new")
@@ -22,5 +24,12 @@ class MemberController(val memberService: MemberService) {
         memberService.join(member)
 
         return "redirect:/"
+    }
+
+    @GetMapping("/members")
+    fun list(model: Model): String {
+        val members = memberService.findMembers()
+        model["members"] = members
+        return "members/member-list"
     }
 }
