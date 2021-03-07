b6907b091e23d2ebafd9341397f5646766b35b0d
soohoonlee <jjinimania@gmail.com>
2021-03-08 07:46:54 +0900

    spring data jpa

--- src/main/kotlin/hello/hellospring/SpringConfig.kt
+++ src/main/kotlin/hello/hellospring/SpringConfig.kt
@@ -1,22 +1,15 @@
 package hello.hellospring
 
-import hello.hellospring.repository.JpaMemberRepository
 import hello.hellospring.repository.MemberRepository
 import hello.hellospring.service.MemberService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import javax.persistence.EntityManager
 
 @Configuration
-class SpringConfig(val em: EntityManager) {
+class SpringConfig(private val memberRepository: MemberRepository) {
 
     @Bean
     fun memberService(): MemberService {
-        return MemberService(memberRepository())
-    }
-
-    @Bean
-    fun memberRepository(): MemberRepository {
-        return JpaMemberRepository(em)
+        return MemberService(memberRepository)
     }
 }
