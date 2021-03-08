f8116cb6e095a6aafc271b65aa124b0a30733e39
이수훈 <sh.lee@newploy.net>
2021-03-08 13:47:27 +0900

    aop 상황

--- src/main/kotlin/hello/hellospring/service/MemberService.kt
+++ src/main/kotlin/hello/hellospring/service/MemberService.kt
@@ -10,11 +10,18 @@ class MemberService(private val memberRepository: MemberRepository) {
     회원 가입
      */
     fun join(member: Member): Long {
-        // 중복 회원 검증
-        validateDuplicateMember(member)
+        val start = System.currentTimeMillis()
+        try {
+            // 중복 회원 검증
+            validateDuplicateMember(member)
 
-        memberRepository.save(member)
-        return member.id
+            memberRepository.save(member)
+            return member.id
+        } finally {
+            val finish = System.currentTimeMillis()
+            val timeMs = finish - start
+            println("join = ${timeMs}ms")
+        }
     }
 
     private fun validateDuplicateMember(member: Member) {
@@ -25,7 +32,14 @@ class MemberService(private val memberRepository: MemberRepository) {
     전체 회원 조회
      */
     fun findMembers(): List<Member?> {
-        return memberRepository.findAll()
+        val start = System.currentTimeMillis()
+        try {
+            return memberRepository.findAll()
+        } finally {
+            val finish = System.currentTimeMillis()
+            val timeMs = finish - start
+            println("findMembers = ${timeMs}ms")
+        }
     }
 
     fun findOne(memberId: Long): Member? {
